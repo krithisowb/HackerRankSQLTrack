8dcdf844ba65185e81e2f61571cdf2075521efef
Krithicka Sivarajan <krithisowb@gmail.com>
2018-05-03 00:34:46 +0530

    Modified incomplete sql scripts with correct solutions

--- Weather Observation Station 5/Solution.sql
+++ Weather Observation Station 5/Solution.sql
@@ -1,10 +1,2 @@
-
-
-
-select * from(select distinct city,length(city) from station order by length(city) asc,city asc) where rownum=
- union
-
-
-
-select * from(select distinct city,length(city) from station order by length(city) desc,city desc) where rownum=
-;
+select * from(select distinct city,length(city) from station order by length(city) asc,city asc) where rownum=1 union
+select * from(select distinct city,length(city) from station order by length(city) desc,city desc) where rownum=1;
